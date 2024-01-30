4a86756bc18c3df683ad9d165127a1363d4064a3
HanBart <125204340+HanBart@users.noreply.github.com>
2024-01-31 01:52:09 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -4,7 +4,7 @@
 module.exports = (req, res) => {
   async fetch(req, env) {
     let url = new URL(req.url);
-    url.hostname = "translate.googleapis.com";
+    url.hostname = "https://translate.googleapis.com/";
     return fetch(new Request(url, req));
   },
 };
