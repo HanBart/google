397314f52f97566fc49e5cde6e9390c4b485b4ae
HanBart <125204340+HanBart@users.noreply.github.com>
2024-01-31 01:59:02 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,23 +1,6 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
-
-module.exports = (req, res) => {
-  let target = "https://translate.googleapis.com/";//your website url
-  //   if (
-  //     req.url.startsWith("/api") ||
-  //     req.url.startsWith("/auth") ||
-  //     req.url.startsWith("/banner") ||
-  //     req.url.startsWith("/CollegeTask")
-  //   ) {
-  //     target = "http://106.15.2.32:6969";
-  //   }
-
-  createProxyMiddleware({
-    target,
-    changeOrigin: true,
-    pathRewrite: {
-      // rewrite request path `/backend`
-      //  /backend/user/login => http://google.com/user/login
-      //   "^/backend/": "/",
-    },
-  })(req, res);
-};
+{
+  "version": 2,
+  "routes": [
+    {"src": "/(.*)","dest": "https://translate.googleapis.com/$1"}
+  ]
+}
