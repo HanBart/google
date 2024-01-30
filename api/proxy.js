e6e4dc6a5b46ee8f1d7601a9f3e9b6e010cee410
HanBart <125204340+HanBart@users.noreply.github.com>
2024-01-31 01:46:37 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,20 +1,10 @@
-const { createProxyMiddleware } = require("http-proxy-middleware");
 
-res=async fetch(req, env) {
+
+
+module.exports = (req, res) => {
+  async fetch(req, env) {
     let url = new URL(req.url);
     url.hostname = "translate.googleapis.com";
     return fetch(new Request(url, req));
   },
-module.exports = (req, res) => {
-  let target = "translate.googleapis.com";//your website url
-
-  createProxyMiddleware({
-    target,
-    changeOrigin: true,
-    pathRewrite: {
-      // rewrite request path `/backend`
-      //  /backend/user/login => http://google.com/user/login
-      //   "^/backend/": "/",
-    },
-  })(req, res);
 };
